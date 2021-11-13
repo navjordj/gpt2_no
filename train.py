244a0550971888e30dfe3657bb270bcb771da1b8
Jørgen Navjord <navjordj@gmail.com>
2021-11-13 14:58:52 +0100

    yuh

--- train.py
+++ train.py
@@ -65,28 +65,44 @@ def main():
 
     block_size = config.max_position_embeddings
 
-    def group_texts(examples):
-        # Concatenate all texts.
-        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-        if total_length >= block_size:
-            total_length = (total_length // block_size) * block_size
-        # Split by chunks of max_len.
-        result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-            for k, t in concatenated_examples.items()
-        }
-        result["labels"] = result["input_ids"].copy()
-        return result
+    print("-------- grouping dataset --------")
+
+    if not os.path.exists("grouped_dataset.pkl"):
 
 
-    lm_datasets = lm_datasets.map(
-        group_texts,
-        batched=True,
-        # num_proc= None,
-    )
+
+        def group_texts(examples):
+            # Concatenate all texts.
+            concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+            total_length = len(concatenated_examples[list(examples.keys())[0]])
+            # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+            # customize this part to your needs.
+            if total_length >= block_size:
+                total_length = (total_length // block_size) * block_size
+            # Split by chunks of max_len.
+            result = {
+                k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+                for k, t in concatenated_examples.items()
+            }
+            result["labels"] = result["input_ids"].copy()
+            return result
+
+
+        lm_datasets = lm_datasets.map(
+            group_texts,
+            batched=True,
+            # num_proc= None,
+        )
+
+        with open("grouped_dataset.pkl", "wb") as f:
+            pickle.dump(lm_datasets, f)
+
+    else:
+        print("grouped dataset on path, loading tokenized dataset")
+
+        with open("grouped_dataset.pkl", "rb") as f:
+            lm_datasets = pickle.load(f)
+
 
 if __name__ == '__main__':
     main()
