de5ed8d7904e653ff2905e9a1f79df659d9ad082
Jørgen Navjord <navjordj@gmail.com>
2021-11-17 11:05:01 +0000

    mhhm

--- train.py
+++ train.py
@@ -41,8 +41,8 @@ import pickle
 SEED=42
 
 num_train_epochs = 20
-per_device_train_batch_size = 64
-per_device_eval_batch_size = 64
+per_device_train_batch_size = 32
+per_device_eval_batch_size = 32
 
 warmup_steps = 1000
 learning_rate = 5e-3
@@ -85,6 +85,21 @@ class TrainState(train_state.TrainState):
 
     def replicate(self):
         return jax_utils.replicate(self).replace(dropout_rng=shard_prng_key(self.dropout_rng))
+
+def write_train_metric(summary_writer, train_metrics, train_time, step):
+    summary_writer.scalar("train_time", train_time, step)
+
+    train_metrics = get_metrics(train_metrics)
+    for key, vals in train_metrics.items():
+        tag = f"train_{key}"
+        for i, val in enumerate(vals):
+            summary_writer.scalar(tag, val, step - len(vals) + i + 1)
+
+def write_eval_metric(summary_writer, eval_metrics, step):
+    for metric_name, value in eval_metrics.items():
+        summary_writer.scalar(f"eval_{metric_name}", value, step)
+
+
 
 def main():
 
